7baa74cbdb3f833d8a42d028d098a72dddbf1b85
korken <151646635+kjdutaro@users.noreply.github.com>
2025-03-23 22:51:49 +0800

    sync

--- backend/main.py
+++ backend/main.py
@@ -319,3 +319,37 @@ plt.title("Model Evaluation Metrics Comparison", pad=20)
 plt.tight_layout()
 plt.savefig(f"{SAVE_DIR}/evaluation_metrics_table.png", bbox_inches='tight', dpi=300)
 plt.show()
+
+
+# ------------------ FastAPI ------------------
+from src.scripts.predict import predict, predict_from_csv, predict_next_month
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+class PredictionRequest(BaseModel):
+    past_values: list[float]
+
+app = FastAPI()
+# ------------------ CORS ------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to restrict origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ------------------ Predict Endpoint ------------------
+@app.post("/predict")
+def predict_endpoint(request: PredictionRequest):
+    return predict(request.past_values)
+
+# ------------------ Predict CSV Endpoint ------------------
+@app.get("/predict-csv")
+def predict_csv_endpoint():
+    return predict_from_csv()
+
+# ------------------ Predict Next Month Endpoint ------------------
+@app.get("/predict-next-month")
+def predict_next_month_endpoint():
+    return predict_next_month()
