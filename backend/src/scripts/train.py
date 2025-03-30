e68b16679fc6d326d428723d0e7ccb71952e5fe3
korken <151646635+kjdutaro@users.noreply.github.com>
2025-03-31 02:11:38 +0800

    test 5 results

--- backend/src/scripts/train.py
+++ backend/src/scripts/train.py
@@ -54,13 +54,13 @@ PATIENCE = 15
 DROPOUT = 0.2
 
 # change for testing; refer to folders in saved_models readme.txt for epochs and learning rate
-EPOCHS = 80
-LEARNING_RATE = 0.0001
+EPOCHS = 90
+LEARNING_RATE = 0.001
 
 # system config
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # change for training; refer to folders in saved_models for folder names
-SAVE_DIR = "saved_models/TEST4/"
+SAVE_DIR = "saved_models/TEST5/"
 os.makedirs(SAVE_DIR, exist_ok=True)
 
 # save config to json
