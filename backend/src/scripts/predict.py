146b1a018184aedb34cb8345a58af984538b69ca
charles787205 <j.canedo.527294@umindanao.edu.ph>
2025-04-01 18:30:34 +0800

    Added actual and dataset prediction

--- backend/src/scripts/predict.py
+++ backend/src/scripts/predict.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import MinMaxScaler
 from src.models.custom_lstm import CustomLSTM
 
 # Configurations
-ELU_MODEL_PATH = "saved_models/model_elu.pth"
-TANH_MODEL_PATH = "saved_models/model_tanh.pth"
+ELU_MODEL_PATH = "saved_models/Original_model/model_elu.pth"
+TANH_MODEL_PATH = "saved_models/Original_model/model_tanh.pth"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 SEQ_LEN = 60
 PRED_DAYS = 10  # Number of days to predict
@@ -99,7 +99,7 @@ def load_data():
 
 
 def predict_from_csv():
-    df, scaled_prices, scaler = load_data()
+    _, scaled_prices, scaler = load_data()
 
     # Get the last 60 closing prices
     if len(scaled_prices) < SEQ_LEN:
@@ -116,9 +116,10 @@ def predict_from_csv():
     return {"predicted_value": float(pred_rescaled)}
 
 
-"""
 def predict_next_month():
     df, scaled_prices, scaler = load_data()
+    df = pd.read_csv("datasets/air_liquide.csv")
+    ochlv_prices = df.values
 
     # Get the last 60 closing prices
     if len(scaled_prices) < SEQ_LEN:
@@ -128,35 +129,7 @@ def predict_next_month():
     X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
 
     future_predictions = []
-
-    for _ in range(PRED_DAYS):
-        with torch.no_grad():
-            elu_+pred_scaled =elu_lstm(X_input).cpu().numpy().flatten()[0]
-
-        pred_rescaled = scaler.inverse_transform([[pred_scaled]])[0][0]
-        future_predictions.append(pred_rescaled)
-
-        # Update input sequence by removing the oldest value and adding the new prediction
-        latest_sequence = np.append(
-            latest_sequence[:, 1:, :], [[[pred_scaled]]], axis=1
-        )
-        X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
-
-    return {"predicted_values": future_predictions}"""
-# predict with elu and tanh
-
-
-def predict_next_month():
-    df, scaled_prices, scaler = load_data()
-
-    # Get the last 60 closing prices
-    if len(scaled_prices) < SEQ_LEN:
-        return {"error": f"Not enough data. Need at least {SEQ_LEN} days."}
-
-    latest_sequence = scaled_prices[-SEQ_LEN:].reshape(1, SEQ_LEN, 1)
-    X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
-
-    future_predictions = []
+    # get the closing prices from the last 60 days
 
     for _ in range(PRED_DAYS):
         with torch.no_grad():
@@ -172,8 +145,65 @@ def predict_next_month():
 
         # Update input sequence by removing the oldest value and adding the new prediction
         latest_sequence = np.append(
-            latest_sequence[:, 1:, :], [[[elu_pred_scaled]]], axis=1
+            latest_sequence[:, 1:, :],
+            [[[scaler.transform([[elu_pred_rescaled]])[0][0]]]],
+            axis=1,
         )
         X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
 
-    return {"predicted_values": future_predictions}
+    return {
+        "predicted_values": future_predictions,
+        "base_data": ochlv_prices[-SEQ_LEN:].tolist(),
+    }
+
+
+def predict_with_dataset():
+    """
+    Leave the latest 10 days for testing and predict the rest
+    """
+
+    df, scaled_prices, scaler = load_data()
+    df = pd.read_csv("datasets/air_liquide.csv")
+    ochlv_prices = df.values
+
+    # Get the last 60 closing prices
+    if len(scaled_prices) < SEQ_LEN:
+        return {"error": f"Not enough data. Need at least {SEQ_LEN} days."}
+
+    # Leave the last 10 days for testing
+    train_data = scaled_prices[:-10]
+
+    # Initialize input sequence with the training data
+    latest_sequence = train_data[-SEQ_LEN:].reshape(1, SEQ_LEN, 1)
+    X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
+
+    future_predictions = []
+    # get the closing prices from the last 60 days
+
+    for _ in range(PRED_DAYS):
+        with torch.no_grad():
+            elu_pred_scaled = elu_lstm(X_input).cpu().numpy().flatten()[0]
+            tanh_pred_scaled = tanh_lstm(X_input).cpu().numpy().flatten()[0]
+
+        elu_pred_rescaled = scaler.inverse_transform([[elu_pred_scaled]])[0][0]
+        tanh_pred_rescaled = scaler.inverse_transform([[tanh_pred_scaled]])[0][0]
+
+        future_predictions.append(
+            {
+                "elu": elu_pred_rescaled,
+                "actual": (ochlv_prices[-PRED_DAYS + _].tolist()[1]),
+            }
+        )
+
+        # Update input sequence by removing the oldest value and adding the new prediction
+        latest_sequence = np.append(
+            latest_sequence[:, 1:, :],
+            [[[scaler.transform([[elu_pred_rescaled]])[0][0]]]],
+            axis=1,
+        )
+        X_input = torch.tensor(latest_sequence, dtype=torch.float32).to(DEVICE)
+
+    return {
+        "predicted_values": future_predictions,
+        "base_data": ochlv_prices[-SEQ_LEN - PRED_DAYS : -PRED_DAYS].tolist(),
+    }
